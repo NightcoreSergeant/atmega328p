a7dd7fddc448d2d4e8b7873f75e2dee49e624456
smile¥ <tomazic.tadej@gmail.com>
2018-12-21 18:54:11 +0000

    Update project.asm

--- project.asm
+++ project.asm
@@ -1,5 +1,16 @@
-;0x2932e0 number of cicles
-ldi r16,0b00000110 ;value for sleep mode...last bit unclear
+;0xa4cb80 number of cicles to be made(24h)
+;0x0000 addres to RESET vector
+.org 0x0
+jmp RESET
+.org 0x034
+RESET:
+        ;
+        ;
+        ;
+
+ldi r16,0b00000111 ;value for sleep mode...last bit enable sleep mode
 out 0x33,r16;address to SMCR-sleep mode control register 0x33(0x55)
-ldi r17,0b000001000 ;value for prescale clock (clk/1024)
+ldi r17,0b100001000 ;value for prescale clock (clk/256)
 sts 0x61, r17 ;address to CLKPR – Clock Prescale Register
+ldi r16,
+sleep

3126ad0e890b4ac5a6d9ef90646f1455e9cc6c1f
smile¥ <tomazic.tadej@gmail.com>
2018-12-26 20:56:50 +0000

    Update project.asm

--- project.asm
+++ project.asm
@@ -1,18 +1,20 @@
-;0xa4cb80 number of cicles to be made(24h)
 ;0x0000 addres to RESET vector
 .org 0x0
 jmp RESET
-.org 0x000E
-jmp TIM1_COMPA
-jmp TIM1_COMPB
+.org 0x001a
+jmp TIMER1_OVF
 .org 0x034
 RESET:
         ;
         ;
         ;
-TIM2_COMPA:
-TIM2_COMPB:
+TIMER1_OVF:
+        cpi r17,0x0
+        breq end_task
+        dec r17
+        iret
 
+.org 0x100
 ldi r16,0b00000111 ;value for sleep mode...last bit enable sleep mode
 out 0x33,r16;address to SMCR-sleep mode control register 0x33(0x55)
 ;system clock;ldi r17,0b100001000 ;value for prescale clock (clk/256)
@@ -24,9 +26,18 @@ out 0x35,r16 ;address to MCUCR
 ldi r16,0b00000101 ;value for prescale clock(1) (clk/1024)
 sts 0x81,r16;address to TC1 Control Register B-TCCR1B
 
-ldi r16,0b00100110 ;value for Timer/Counter 1 Interrupt Mask Register TIMSK1 (enable A,B interrupts 
+ldi r16,0b00100111 ;value for Timer/Counter 1 Interrupt Mask Register TIMSK1 (enable overflow interrupts and A,B comparison) 
 sts 0x6f,r16 ;address to Timer/Counter 1 Interrupt Mask Register
 
+ldi r17,0x29;value to decrese for pretty acurate time (a day)
 
+loop:
+        sei
+        sleep
+        cli
+        rjmp loop
 
-sleep
+end_task:
+        cli
+        nop
+        RESET

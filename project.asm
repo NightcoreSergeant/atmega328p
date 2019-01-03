bab223b3c2bff0c470cf334fb293d889b1278c19
smile¥ <tomazic.tadej@gmail.com>
2019-01-03 15:45:50 +0100

    Update project.asm

--- project.asm
+++ project.asm
@@ -1,29 +1,52 @@
 ;0x0000 addres to RESET vector
 .org 0x0
 jmp RESET
+
 .org 0x001a
 jmp TIMER1_OVF
+
 .org 0x034
 RESET:
-        ;
-        ;
-        ;
+	jmp start
+
 TIMER1_OVF:
-        cpi r17,0x0
-        breq end_task
-        dec r17
-        iret
+    cpi r17,0x0
+    breq IF
+    dec r17
+	cbi portb, 5
+    reti
+IF:
+	jmp end_task
+
+;.include "knjiznica.asm"
+
+;.cseg
 
 .org 0x100
+
+start: 
+ldi r16, 0xff ;close light
+out ddrb, r16
+sbi portb, 5
+
+call setupUart
+
+;ldi r16, 'A'
+;call send_char
+
+
+cli
 ldi r16,0b00000111 ;value for sleep mode...last bit enable sleep mode
-out 0x33,r16;address to SMCR-sleep mode control register 0x33(0x55)
+out 0x33,r16 ;address to SMCR-sleep mode control register 0x33(0x55)
 ;system clock;ldi r17,0b100001000 ;value for prescale clock (clk/256)
 ;sysetm clock;sts 0x61, r17 ;address to CLKPR – Clock Prescale Register
 
 ldi r16,0b0110000 ;value for BOD disable-BODS and BODSE
 out 0x35,r16 ;address to MCUCR
 
-ldi r16,0b00000101 ;value for prescale clock(1) (clk/1024)
+;ldi r16,0b00000101 ;value for prescale clock(1) (clk/1024)
+ldi r16,0b00000001 ;value for prescale clock(1) (clk/1024)
+
 sts 0x81,r16;address to TC1 Control Register B-TCCR1B
 
 ldi r16,0b00100111 ;value for Timer/Counter 1 Interrupt Mask Register TIMSK1 (enable overflow interrupts and A,B comparison) 
@@ -32,12 +55,16 @@ sts 0x6f,r16 ;address to Timer/Counter 1 Interrupt Mask Register
 ldi r17,0x29;value to decrese for pretty acurate time (a day)
 
 loop:
-        sei
-        sleep
-        cli
-        rjmp loop
+	sei
+	sleep
+    cli
+    rjmp loop
 
 end_task:
-        cli
-        nop
-        RESET
+    cli
+    nop ;some magic to open electricity
+    jmp RESET
+
+
+.cseg
+hello: .db "Test",0

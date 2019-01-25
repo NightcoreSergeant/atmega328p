df912d5308dd837c3a0a11fa1dc8d00cdacf4992
smile¥ <tomazic.tadej@gmail.com>
2019-01-25 20:09:03 +0100

    Update project.asm

--- project.asm
+++ project.asm
@@ -9,17 +9,24 @@ jmp prekinitev
 RESET:
 	sbi ddrb,5
 	sbi portb,5
+
+lds r16,PRR ;all kind of power save
+ori r16,0b10101111
+sts PRR,r16
+
 in r16,0x5
-lds r16, TIMSK2
+lds r16, TIMSK2 ;enable timer2
 ori r16,1<<TOIE2
 sts TIMSK2,r16
 
-lds r16, TCCR2B
+lds r16, TCCR2B ;prescaler
 andi r16,0b11111000
 ori r16,0b00000111
 sts TCCR2B,r16
 
-
+lds r16,MCUCR ;disable BOD
+ori r16,0b01000000
+sts MCUCR,r16
 
 
 lds r16,0x53 ;sleep mode
@@ -28,9 +35,9 @@ ori r16,0b00000111
 sts 0x53,r16
 
 sei
-ldi r29,0x30
+ldi r29,0x54
 ldi r30,0xff
-ldi r31,0xff ;final
+ldi r31,0xff ;calculations for a day
 
 
 main:
@@ -42,22 +49,22 @@ main:
 prekinitev:
 	cli
 	dec r31
-	cpi r31,0x0    ;cakanje na sec
+	cpi r31,0x0    ;cakanje na 4sec
 	brne weit
 	
 	ldi r31,0xff
 	
-	dec r30
-	cpi r30,0x0 ;cakanje na pol ure
-	brne weit
+;	dec r30
+;	cpi r30,0x0 ;cakanje na okol 17min
+;	brne weit
 	
-	ldi r30,0xff
+;	ldi r30,0xff
 
-	dec r29
-	cpi r29,0x0 ;waiting whole day
-	brne weit
+;	dec r29
+;	cpi r29,0x0 ;waiting whole day
+;	brne weit
 
-	ldi r29,0x30
+;	ldi r29,0x54
 
 
 	in r16,0x5 
